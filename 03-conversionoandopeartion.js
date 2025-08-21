fb3fd551aa28b3420b838324cc00c8a568c4200b
kartiksharma76 <kartiksharm768976@gmail.com>
2025-08-21 15:33:20 +0530

    type of conversion.added

---
+++ 03-conversionoandopeartion.js
@@ -0,0 +1,26 @@
+let score= "Kartik"
+
+//console.log(typeof score);
+//console.log(typeof (score))
+
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);// NaN not a number
+
+
+//"33" => 33
+// "33abc" => NaN
+// true=>1; false => 0
+
+let isLoggedIn ="Kartik"
+
+let booleanisLoggedIn =Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+// 1=> true; 0 =>false
+// "" =>false
+// "kartik" =>true
+let someNumber =33
+
+let stringNumber=String(someNumber)
+console.log( typeof stringNumber);
