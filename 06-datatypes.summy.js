12e4d75c75a43edc59ccd842060f1c114367cd1a
kartiksharma76 <kartiksharm768976@gmail.com>
2025-08-22 08:52:12 +0530

    summarydatatye.added

---
+++ 06-datatypes.summy.js
@@ -0,0 +1,35 @@
+// primitive data types
+
+// 7 types : String , Number ,Boolean, null, undefined, Symbol,  BigInt
+
+const score =100
+const scoreValue =100.3
+
+const isLoggedIn =false
+const outsideTemp =null
+let useEmial;
+
+const id= Symbol('123')
+const anotherId= Symbol('123')
+
+console.log(id===anotherId);
+
+//const bigNumber = 34345654345654334543n
+
+
+
+// Reference (Non primitive)
+
+// Arrays ,Objects, Functions
+
+const heros =["shaktiman","nagraj","doga"]
+let myObj={
+    name :"kartik",
+    age :21,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+console.log(typeof anotherId)//atma scripts
