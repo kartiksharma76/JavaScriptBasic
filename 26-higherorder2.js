a663207aeb633262613996fddcca855bbac3f31f
kartik <kartiksharma768976@gmail.com>
2025-09-01 21:51:43 +0530

    js.added

---
+++ 26-higherorder2.js
@@ -0,0 +1,31 @@
+const myObject = {
+    js: 'java scripts',
+    cpp:'c++',
+    rb:"ruby",
+    swift : "swift by apple"
+}
+for (const key in myObject) {
+   // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+const programming = ["js" ,"rb ", "py", "java","cpp"]
+for (const key in programming) {
+  // console.log(programming[key]);
+   
+}
+
+
+// Maps => unique value
+
+// const map = new Map()
+// map.set('IN',"India")
+// map.set('USA' ,"United States of America")
+// map.set('FR',"France")
+
+// for (const key in map) {
+//    console.log(key);
+   
+    
+// }
+
